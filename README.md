2236b0952d528a9a48aa7b458ad041e729d01e3b
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2024-01-15 18:44:44 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Example_Library-Morse
+ Following the tutorial library from Arduino.
---
+++ libraries/Morse.h
@@ -0,0 +1,23 @@
+/*
+  Morse.h - Library for flashing Morse code.
+  Created by David A. Mellis, November 2, 2007.
+  Released into the public domain.
+*/
+#ifndef Morse_h
+#define Morse_h
+
+#include "Arduino.h"
+
+class Morse
+{
+  public:
+    Morse(int pin);
+    void begin();
+    void dot();
+    void dash();
+  private:
+    // _ is common practice to denote private variables
+    int _pin;
+};
+
+#endif
